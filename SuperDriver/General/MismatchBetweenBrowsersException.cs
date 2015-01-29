cd6f2383091a12197d8a5d92fb53963742e4520d
Steve Fenton <commits@stevefenton.co.uk>
2015-01-29 22:21:19 +0000

    For issue #2 - additional information on MismatchException.

--- SuperDriver/General/MismatchBetweenBrowsersException.cs
+++ SuperDriver/General/MismatchBetweenBrowsersException.cs
@@ -1,11 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Security.Permissions;
 
 namespace Fenton.Selenium.SuperDriver
 {
     [Serializable]
     public class MismatchBetweenBrowsersException<T> : SuperException
     {
-        public MismatchBetweenBrowsersException(IEnumerable<T> values) : base(string.Format("Not all elements match: {0}", string.Join(",", values))) { }
+        public IEnumerable<T> Values { get; private set; }
+
+        public MismatchBetweenBrowsersException(IEnumerable<T> values)
+            : base(string.Format("Not all elements match: {0}", string.Join(",", values)))
+        {
+            Values = values;
+        }
+
+        [SecurityPermission(SecurityAction.Demand, SerializationFormatter = true)]
+        public virtual void GetObjectData(SerializationInfo serializationInfo, StreamingContext context)
+        {
+            if (serializationInfo == null)
+            {
+                throw new ArgumentNullException("info");
+            }
+
+            serializationInfo.AddValue("Values", Values);
+
+            base.GetObjectData(serializationInfo, context);
+        }
     }
 }
